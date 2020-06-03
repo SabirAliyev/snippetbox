ff96edad933b8c4b1be07a981486bfb64c7e51fb
Sabir <sabir.aliyev.itdep#gmail.com>
2020-06-03 16:57:41 +0400

    First hanlers added.

--- main.go
+++ main.go
@@ -8,7 +8,19 @@ import (
 // Define a home handler function which writes a byte slice containing
 // "Hello from Snippetbox" as the response body.
 func home(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main(){
@@ -16,6 +28,8 @@ func main(){
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	// Use the http.ListenAndServe() function to start a new web server. We pass in
 	// two parameters: the TCP network address to listen on (in this case ":4000")
@@ -26,3 +40,6 @@ func main(){
 	err :=http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
+
+
+
