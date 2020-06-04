1a29171d2d7a583c67c4dff50e66de06130c5511
SabirAliyev <sabir.aliyev.itdep@gmail.com>
2020-06-04 17:19:34 +0400

    URL Querry Strings

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Define a home handler function which writes a byte slice containing
@@ -16,7 +18,19 @@ func home(w http.ResponseWriter, r *http.Request){
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Display a specific snippet..."))
+	// Extract the value of the id parameter from the query string and try to
+	// convert it to an integer using the strconv.Atoi() function. If it can't
+	// be converted to an integer, or the value is less than 1, we return a 404 page
+	// not found response.
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+	return
+	}
+
+	// Use the fmt.Fprintf() function to interpolate the id value with our response
+	// and write it to the http.ResponseWriter.
+	fmt.Fprintf(w, "Display a specfic snippet with ID %d...", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request){
@@ -27,12 +41,9 @@ func createSnippet(w http.ResponseWriter, r *http.Request){
 		// response header map. The first parameter is the header name, and
 		// the second parameter is the header value.
 		w.Header().Set("Allow", http.MethodPost)
-		// If it's not, use the w.WriteHeader() method to send a 405 status
-		// code and the w.Write() method to write a "Method Not Allowed"
-		// response body. We then return from the function so that the
-		// subsequent code is not executed.
-		w.WriteHeader(405)
-		w.Write([]byte("Method not allowed."))
+		/// Use the http.Error() function to send a 405 status code and "Method Not
+		// Allowed" string as the response body.
+		http.Error(w, "Method Not Allowed", 405)
 		return
 	}
 	w.Write([]byte("Create a new snippet..."))
