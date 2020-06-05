231b9421777f42d7a3aa5cdf1b025781a32c9db1
SabirAliyev <sabir.aliyev.itdep@gmail.com>
2020-06-05 16:59:56 +0400

    HTML Temlating and Inheritance

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
 
 func main() {
@@ -13,34 +11,12 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("snippet/create", createSnippet)
 
-	log.Println("Starting server on :40000")
+	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
 
-func home(w http.ResponseWriter, r *http.Request){
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	w.Write([]byte("Hello from snippetbox!"))
-}
 
-func showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-	fmt.Fprintf(w, "Display a specific snippet ID %d...", id)
-}
 
-func createSnippet(w http.ResponseWriter, r *http.Request){
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method not Allowed", 405)
-		return
-	}
-	w.Write([]byte("Create a new snippet..."))
-}
+
 
