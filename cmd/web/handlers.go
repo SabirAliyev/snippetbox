3e2d67891b77bb9a1e4e16b0ad5e3ac07996f577
SabirAliyev <sabir.aliyev.itdep@gmail.com>
2020-06-08 16:12:27 +0400

    Commandline flags

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -31,7 +31,7 @@ func home(w http.ResponseWriter, r *http.Request){
 		return
 	}
 
-	// We then use the Execute() method on the template set to write the template
+	// We use the Execute() method on the template set to write the template
 	// content as the response body. The last parameter to Execute() represents any
 	// dynamic data that we want to pass in, which for now we'll leave as nil.
 	err = ts.Execute(w, nil)
