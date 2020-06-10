38f3a4248858abb2f5460972785f497cd42d4e8a
SabirAliyev <sabir.aliyev.itdep@gmail.com>
2020-06-10 16:56:22 +0400

    Centralized Error Handling

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,9 +1,8 @@
 package main
 
-import(
+import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -24,25 +23,20 @@ func (app *application) home(w http.ResponseWriter, r *http.Request){
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		// Because the home handler function is now a method against application
-		// it can access its fields, including the error logger. We'll write the log
-		// message to this instead of the standard logger.
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err)		// Use the serverError() helper.
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err) 	// Use the serverError() helper.
 	}
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)		// Use the notFound() helper.
 		return
 	}
 	fmt.Fprintf(w, "Display a specific snippet ID %d...", id)
@@ -51,7 +45,7 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request){
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method not Allowed", 405)
+		app.clientError(w, http.StatusMethodNotAllowed) 	// Use the clientError() helper.
 		return
 	}
 	w.Write([]byte("Create a new snippet..."))
