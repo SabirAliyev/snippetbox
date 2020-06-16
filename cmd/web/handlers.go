abee8c818a1ad0d4e4ccbc4251aad34c56e19aa4
SabirAliyev <sabir.aliyev.itdep@gmail.com>
2020-06-16 17:17:53 +0400

    Single-record SQL Queries.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
+	"sabiraliyev.net/snippetbox/pkg/models"
 	"strconv"
+
+
 )
 
 // Change the signature of the home handler so it is defined as a method against
@@ -39,7 +43,21 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)		// Use the notFound() helper.
 		return
 	}
-	fmt.Fprintf(w, "Display a specific snippet ID %d...", id)
+
+	// Use the SnippetModel object's Get method to retrieve the data for a
+	// specific record based on its ID. If no matching record is found,
+	// return a 404 Not Found response.
+	s, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNorecord){
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	fmt.Fprintf(w, "%v", s)
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request){
@@ -49,19 +67,4 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request){
 		return
 	}
 
-	// Create some variables holding dummy data. We'll remove these later on
-	// during the build.
-	title := "0 snail"
-	content := "O snail\\nClimb Mount Fuji,\\nBut slowly, slowly!\\n\\n– Kobayashi Issa"
-	expires := "7"
-
-	// Pass the data to the SnippetModel.Insert() method, receiving the
-	// ID of the new record back.
-	id, err := app.snippets.Insert(title, content, expires)
-	if err != nil {
-		app.serverError(w, err)
-	}
-
-	// Redirect the user to the relevant page for the snippet.
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 }
