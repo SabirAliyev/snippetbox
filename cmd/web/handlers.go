5c6eed68351b94f447cc5ddab7d5a1572f33fccb
SabirAliyev <sabir.aliyev.itdep@gmail.com>
2020-06-18 16:59:14 +0400

    Tamplate Actions and Functions

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"errors"
-	"fmt"
+	// "errors"
+	// "fmt"
 	"html/template"
 	"net/http"
 	"strconv"
 
-	"sabiraliyev.net/snippetbox/pkg/models"
+	// "sabiraliyev.net/snippetbox/pkg/models"
 
 )
 
@@ -25,30 +25,10 @@ func (app *application) home(w http.ResponseWriter, r *http.Request){
 		return
 	}
 
-	for _, snippet := range s{
-		fmt.Fprintf(w, "%v\n", snippet)
-	}
-}
-
-func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		app.notFound(w)		// Use the notFound() helper.
-		return
-	}
-
-	s, err := app.snippets.Get(id)
-	if err != nil {
-		if errors.Is(err, models.ErrNorecord){
-			app.notFound(w)
-		} else {
-			app.serverError(w, err)
-		}
-		return
-	}
+	data := &templateData{Snippets: s}
 
 	files := []string{
-		"./ui/html/show.page.tmpl",
+		"./ui/html/home.page.tmpl",
 		"./ui/html/base.layout.tmpl",
 		"./ui/html/footer.partial.tmpl",
 	}
@@ -62,9 +42,18 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 
 	// Add and then execute them. Notice how we are passing in the snippet
 	// data (a model.Snippet struct) as the final parameter.
-	err = ts.Execute(w, s)
+	err = ts.Execute(w, data)
 	if err != nil {
 		app.serverError(w, err)
+	}
+
+}
+
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)		// Use the notFound() helper.
+		return
 	}
 }
 
