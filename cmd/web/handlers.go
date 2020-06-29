7db10c889d805b21d5fe80be82e7f3c859369b87
SabirAliyev <sabir.aliyev.itdep@gmail.com>
2020-06-29 14:09:39 +0400

    Scaling Data Validation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,18 +5,12 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"strings"
-	"unicode/utf8"
 
+	"sabiraliyev.net/snippetbox/pkg/forms"
 	"sabiraliyev.net/snippetbox/pkg/models"
 )
 
-// Change the signature of the home handler so it is defined as a method against
-// *application.
 func (app *application) home(w http.ResponseWriter, r *http.Request){
-	// Because Pat matches the "/" path exactly, we can now remove the manual check
-	// of r.URL.Path != "/" from this handler.
-
 	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -29,8 +23,6 @@ func (app *application) home(w http.ResponseWriter, r *http.Request){
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	// Pat doesn't strip the colon from the named capture key, so we need to
-	// get the value of ":id" from the query string instead of "id".
 	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
@@ -42,7 +34,7 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		if errors.Is(err, models.ErrNorecord) {
 			app.notFound(w)
 		} else {
-			app.serverError(w, err)
+
 		}
 		return
 	}
@@ -53,7 +45,10 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request){
-	app.render(w, r, "create.page.tmpl", nil)
+	app.render(w, r, "create.page.tmpl", &templateData{
+		// Pass a new empty forms.Form object to the template.
+		Form: forms.New(nil),
+	})
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request){
@@ -63,40 +58,24 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request){
 		return
 	}
 
-	title := r.PostForm.Get("title")
-	content := r.PostForm.Get("content")
-	expires := r.PostForm.Get("expires")
+	// Create  a new form.Form struct containing the POSTed data from the
+	// form, then use the validation method to check the content.
+	form := forms.New(r.PostForm)
+	form.Required("title", "content", "expires")
+	form.MaxLength("title", 100)
+	form.PermittedValue("expires", "365", "7", "1")
 
-	// Initialize a map to hold any validation errors.
-	errors := make(map[string]string)
-
-	if strings.TrimSpace(title) == "" {
-		errors["title"] = "This field cannot be blank"
-	} else if utf8.RuneCountInString(title) > 100 {
-		errors["title"] = "This field is too long (maximum is 100 characters)"
-	}
-
-	if strings.TrimSpace(content) == "" {
-		errors["content"] = "This field cannot be blank"
-	}
-
-	if strings.TrimSpace(expires) == "" {
-		errors["expires"] = "This field cannot be blank"
-	} else if expires != "365" && expires != "7" && expires != "1" {
-		errors["expires"] = "This field is invalid"
-	}
-
-	// If there are any validation errors, re-display the create.page.tmpl
-	// template passing in the validation errors and previously submitted r.PostForm data.
-	if len(errors) > 0 {
-		app.render(w, r, "create.page.tmpl", &templateData{
-			FormErrors: errors,
-			FormData: r.PostForm,
-		})
+	// If the form isn`t valid, redisplay the template passing in the
+	// form.Form object as the data.
+	if !form.Valid() {
+		app.render(w, r, "create.page.tmpl", &templateData{Form: form})
 		return
 	}
 
-	id, err := app.snippets.Insert(title, content, expires)
+	// Because the form data (with type url.Values) has been anonymously embedded
+	// in the form.Form struct, we can use the Get() method to retrieve
+	// the validated value for a particular form field.
+	id, err := app.snippets.Insert(form.Get("title"), form.Get("content"), form.Get("expires"))
 	if err != nil {
 		app.serverError(w, err)
 		return
