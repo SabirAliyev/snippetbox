b7e3de9fef132bb3bd412337bafdc12b86f4dd76
SabirAliyev <sabir.aliyev.itdep@gmail.com>
2020-07-08 17:08:42 +0400

    coolies and Redirections

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -4,6 +4,7 @@ import(
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
 	"net/http/httptest"
 	"testing"
 )
@@ -17,21 +18,33 @@ func newTestApplication(t *testing.T) *application {
 	}
 }
 
-// Define a custom testServer type which anonymously embeds a httptest.Server instance.
 type testServer struct {
 	*httptest.Server
 }
 
-// Create a newTestServer helper which initalizes and returns a new instance
-// of our custom testServer type.
 func newTestServer(t *testing.T, h http.Handler) *testServer {
 	ts := httptest.NewTLSServer(h)
+
+	// Initialize a new cookie jar.
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Add the cookie jar to the client, so that response cookies are stored
+	// and then sent with subsequent requests.
+	ts.Client().Jar = jar
+
+	// Disable redirect-following for the client. Essentially this function
+	// is called after a 3xx response is received by the client, and returning
+	// the http.ErrUseLastResponse error forces it to immediately return the
+	// received response.
+	ts.Client().CheckRedirect = func(req *http.Request, via[]*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
 	return &testServer{ts}
 }
 
-// Implement a get method on our custom testServer type. This makes a GET
-// request to a given url path on the test server, and returns the response
-// status code, headers and body.
 func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, []byte) {
 	rs, err := ts.Client().Get(ts.URL + urlPath)
 	if err != nil {
